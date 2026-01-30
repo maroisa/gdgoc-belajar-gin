adf7e3cdf73bbe0cc5dc0128fd4e4c31d302b2af
maroisa <ahmadroisul412@gmail.com>
2026-01-30 13:38:05 +0700

    add PUT and DELETE method

--- server/note/routes.go
+++ server/note/routes.go
@@ -14,7 +14,8 @@ func (nh *NoteHandler) Routes() {
 	note := nh.r.Group("/note")
 	note.GET("", nh.getNotes)
 	note.POST("", nh.postNotes)
-	note.PUT("/:id", nh.putNotes)
+	note.PUT("/:id", nh.putNote)
+	note.GET("/:id", nh.getNote)
 	note.DELETE("/:id", nh.deleteNotes)
 }
 
@@ -28,6 +29,21 @@ func (nh *NoteHandler) getNotes(c *gin.Context) {
 	}
 	server.Response(c, &server.BaseResponse{
 		Data: notes,
+	})
+}
+
+func (nh *NoteHandler) getNote(c *gin.Context) {
+	user, err := gorm.G[model.Note](nh.db).Where("id = ?", c.Param("id")).First(context.Background())
+	if err != nil {
+		server.Response(c, &server.BaseResponse{
+			Status:  404,
+			Message: "Not Found",
+		})
+		return
+	}
+
+	server.Response(c, &server.BaseResponse{
+		Data: user,
 	})
 }
 
@@ -60,10 +76,64 @@ func (nh *NoteHandler) postNotes(c *gin.Context) {
 	c.JSON(http.StatusOK, note)
 }
 
-func (nh *NoteHandler) putNotes(c *gin.Context) {
+func (nh *NoteHandler) putNote(c *gin.Context) {
+	ctx := context.Background()
+	id := c.Param("id")
+	var note model.Note
 
+	if err := c.ShouldBindJSON(&note); err != nil {
+		server.Response(c, &server.BaseResponse{
+			Status:  http.StatusBadRequest,
+			Message: "Bad Request",
+		})
+		return
+	}
+
+	if note.Judul != "" {
+		_, err := gorm.G[model.Note](nh.db).Where("id = ?", id).Update(ctx, "judul", note.Judul)
+		if err != nil {
+			server.Response(c, &server.BaseResponse{
+				Status:  http.StatusBadRequest,
+				Message: "Bad Request",
+			})
+			return
+		}
+	}
+
+	if note.Deskripsi != "" {
+		_, err := gorm.G[model.Note](nh.db).Where("id = ?", id).Update(ctx, "deskripsi", note.Deskripsi)
+		if err != nil {
+			server.Response(c, &server.BaseResponse{
+				Status:  http.StatusBadRequest,
+				Message: "Bad Request",
+			})
+			return
+		}
+	}
+
+	server.Response(c, &server.BaseResponse{})
 }
 
 func (nh *NoteHandler) deleteNotes(c *gin.Context) {
+	ctx := context.Background()
+	id := c.Param("id")
 
+	rowsAffected, err := gorm.G[model.Note](nh.db).Where("id = ?", id).Delete(ctx)
+	if err != nil {
+		server.Response(c, &server.BaseResponse{
+			Status:  http.StatusBadRequest,
+			Message: "Bad Request",
+		})
+		return
+	}
+
+	if rowsAffected == 0 {
+		server.Response(c, &server.BaseResponse{
+			Status:  404,
+			Message: "Not Found",
+		})
+		return
+	}
+
+	server.Response(c, &server.BaseResponse{})
 }
