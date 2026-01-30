123a3d021ad6460051f01dd57a706a9eb78522e7
maroisa <ahmadroisul412@gmail.com>
2026-01-30 11:40:35 +0700

    add GET and POST method

--- db/model/note.go
+++ db/model/note.go
@@ -4,6 +4,6 @@ import "gorm.io/gorm"
 
 type Note struct {
 	gorm.Model
-	Judul string
-	Deskripsi string
+	Judul     string `form:"judul" json:"judul" binding:"required" gorm:"not null"`
+	Deskripsi string `form:"deskripsi" json:"deskripsi" binding:"required"`
 }
--- server/note/routes.go
+++ server/note/routes.go
@@ -4,6 +4,7 @@ import (
 	"belajar-gin/db/model"
 	"context"
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -11,25 +12,45 @@ import (
 
 func (nh *NoteHandler) Routes() {
 	note := nh.r.Group("/note")
-	note.GET("/", nh.getNotes)
-	note.POST("/", nh.postNotes)
+	note.GET("", nh.getNotes)
+	note.POST("", nh.postNotes)
 	note.PUT("/:id", nh.putNotes)
 	note.DELETE("/:id", nh.deleteNotes)
 }
 
 func (nh *NoteHandler) getNotes(c *gin.Context) {
-	ctx := context.Background()
-
-	notes, err := gorm.G[model.Note](nh.db).Find(ctx)
+	notes, err := gorm.G[model.Note](nh.db).Find(context.Background())
 	if err != nil {
-		log.Println("Failed to get notes: ", err.Error())
+		log.Println("Failed")
+		return
 	}
-
-	log.Println(notes)
+	c.JSON(http.StatusOK, notes)
 }
 
 func (nh *NoteHandler) postNotes(c *gin.Context) {
+	var note model.Note
+	if err := c.ShouldBindJSON(&note); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Bad Request",
+		})
+		return
+	}
 
+	if note.Judul == "" || note.Deskripsi == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Bad Request",
+		})
+		return
+	}
+
+	err := gorm.G[model.Note](nh.db).Create(context.Background(), &note)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Failed to insert note: " + err.Error(),
+		})
+	}
+
+	c.JSON(http.StatusOK, note)
 }
 
 func (nh *NoteHandler) putNotes(c *gin.Context) {
